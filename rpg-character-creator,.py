b75c51d7af90a00b6ce7250de1f3d1dac12a321e
paula-arencibiap <paula.arencibiap@gmail.com>
2026-03-12 21:26:39 -0400

    Add RPG Character Creator - Python

---
+++ rpg-character-creator,.py
@@ -0,0 +1,29 @@
+full_dot = '●'
+empty_dot = '○'
+
+def create_character(character_name, strength, intelligence, charisma):
+    if not isinstance(character_name, str):
+        return "The character name should be a string"
+    if character_name == "":
+        return "The character should have a name"
+    if len(character_name) > 10:
+       return "The character name is too long" 
+    if " " in character_name:
+        return "The character name should not contain spaces"
+    if not isinstance (strength, int) or not isinstance (intelligence, int) or not isinstance (charisma, int):
+        return "All stats should be integers"    
+    if strength < 1 or intelligence < 1 or charisma < 1:
+        return "All stats should be no less than 1"
+    if strength > 4 or intelligence > 4 or charisma > 4:
+        return "All stats should be no more than 4"
+    if strength + intelligence + charisma != 7:
+        return "The character should start with 7 points"
+
+    return character_name + "\n" + "STR " + stat_points(strength) + "\n" + "INT " + stat_points(intelligence) + "\n" + "CHA " + stat_points(charisma)
+    
+def stat_points (stat):
+    points = full_dot * stat
+    empty = empty_dot * (10 - stat)
+    return points + empty 
+
+print(create_character('Paula', 3, 3, 1))
